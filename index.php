e1b93672016b51a99bd9f1c71a8bbb54f2896e05
dreamblyss <iwekuba.henry@gmail.com>
2020-11-25 21:13:48 +0100

    Added login form

--- index.php
+++ index.php
@@ -1,1 +1,58 @@
-<?php echo "Hello World"; ?>
+<!Doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="follow">
+    <title>FRIENDS OF JESUS CHRIST IGM LOGIN SYSTEM</title>
+    <base href="/" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.5.9/css/uikit.min.css" />
+</head>
+
+<body>
+<!--JQuery Required-->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!--UIKits JS-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.5.9/js/uikit-core.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.5.9/js/uikit-icons.min.js"></script>
+
+<div class="uk-section uk-container">
+        <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+        
+        
+<form class="uk-form-stacked js-login">
+
+<!--Email-->
+<div class="uk-margin">
+    <label class="uk-form-label" for="form-stacked-text">Email</label>
+    <div class="uk-form-controls">
+        <input class="uk-input" id="form-stacked-text" type="email" placeholder="email@email.com" required />
+    </div>
+</div>
+
+<!--Password-->
+<div class="uk-margin">
+    <label class="uk-form-label" for="form-stacked-text">Password</label>
+    <div class="uk-form-controls">
+        <input class="uk-input" id="form-stacked-text" type="password" placeholder="Your Password" required />
+    </div>
+</div>
+
+<!--Submit Button-->
+<div class="uk-margin">
+    <button class="uk-button uk-button-default" type="submit">Login</button>
+</div>
+
+
+</form>
+
+        </div>
+
+    </div>
+
+    </body>
+
+</html>
